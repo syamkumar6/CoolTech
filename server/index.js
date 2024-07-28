3030503b4a862c5fd755d2fe535ce8d55b6dd926
Syam <135089954+syamkumar6@users.noreply.github.com>
2024-07-28 18:13:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParses());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://cool-tech.vercel.app", "http://localhost:5173"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
