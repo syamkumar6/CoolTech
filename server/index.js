d99abf6e273121b111bf3cd1bb2d065e062f894c
Syam <135089954+syamkumar6@users.noreply.github.com>
2024-07-28 18:38:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const StocksRouter = require("./routes/StocksRouter")
 const HistoryRouter = require("./routes/historyRouter")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cookieParses());
